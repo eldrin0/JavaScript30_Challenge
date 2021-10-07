0088485efaf7eb1a4cc3a75fe48e8a696b72e2c9
eldrin0 <eldrinereqi0@gmail.com>
2021-10-07 19:03:52 +0200

    added the second challenge code solution

---
+++ 02 - CSS + JS Clock/main.js
@@ -0,0 +1,21 @@
+let secondsHand = document.querySelector('.second-hand');
+let minuteHand = document.querySelector('.minute-hand');
+let hourHand = document.querySelector('.hour-hand');
+
+function setDate() {
+    const now = new Date();
+
+    const second = now.getSeconds();
+    const secondDegree = (second/60)*360 + 90;
+    secondsHand.style.transform = `rotate(${secondDegree}deg)`;
+    
+    const mins = now.getMinutes();
+    const minuteDegree = ((mins/60)*360)+90;
+    minuteHand.style.transform = `rotate(${minuteDegree}deg)`;
+    
+    const hour = now.getHours();
+    const hourDegree = ((hour/12)*360)+90;
+    hourHand.style.transform = `rotate(${hourDegree}deg)`;
+}
+
+setInterval(setDate, 1000);
